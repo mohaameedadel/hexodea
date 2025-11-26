f028e86acc76623da95c837f34b0edff771d0046
mohaameedadel <doooooolaaaaaa@gmail.com>
2025-11-26 23:03:12 +0200

    feat: lottie file

--- app/hexodea/contact-us/contact-us.tsx
+++ app/hexodea/contact-us/contact-us.tsx
@@ -1,3 +1,10 @@
 export default function ContactUs() {
-  return <div>ContactUs</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <iframe
+        className="w-[60vh] h-full"
+        src="https://lottie.host/embed/6ecaebbc-dac6-448b-a0e9-626a7f049bf5/04OyLtTBqa.lottie"
+      ></iframe>
+    </div>
+  );
 }
